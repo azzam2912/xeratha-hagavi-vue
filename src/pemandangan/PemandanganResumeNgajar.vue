83cef50fdc8687b6095c3196d3694a0f2e1e4b86
azzam2912 <azzam.gamers12@gmail.com>
2024-03-11 23:33:38 +0700

    menambahkan google drive  pdf viewer untuk resume

--- src/pemandangan/PemandanganResumeNgajar.vue
+++ src/pemandangan/PemandanganResumeNgajar.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <h2>If the pdf cannot be shown, you can download my latest teaching resume<a href="https://drive.google.com/file/d/1s1RSNqtOwUMTMM7r6ZYVF5tNthsxt8C3/view?usp=drive_link" target="_blank">here!</a></h2>
+        <h3>If the pdf cannot be shown, you can download my latest teaching resume<a href="https://drive.google.com/file/d/1s1RSNqtOwUMTMM7r6ZYVF5tNthsxt8C3/view?usp=drive_link" target="_blank">here!</a></h3>
         Anyway here is the pdf of my resume (it should be shown after a moment):
-        <iframe src="https://drive.google.com/file/d/1s1RSNqtOwUMTMM7r6ZYVF5tNthsxt8C3/preview" width="100%" height="700" allow="autoplay"></iframe>
+        <br />
+        <iframe src="https://drive.google.com/file/d/1s1RSNqtOwUMTMM7r6ZYVF5tNthsxt8C3/preview" width="100%" height="650" allow="autoplay"></iframe>
     </div>
 </template>
 
--- src/pemandangan/PemandanganResumeNgoding.vue
+++ src/pemandangan/PemandanganResumeNgoding.vue
@@ -1,8 +1,9 @@
 <template>
 	<div>
-        <object width="100%" height="700" type="application/pdf" data="src/data/resume-ngoding.pdf">
-            <p>Sorry, the PDF cannot be shown :(</p>
-        </object>
+        <h3>If the pdf cannot be shown, you can download my latest resume on <a href="https://drive.google.com/file/d/1O_OgxQ8aCS0jIWkP140BBkKtNwkfQrEq/view?usp=drive_link" target="_blank">here!</a></h3>
+        Anyway here is the pdf of my resume (it should be shown after a moment):
+        <br />
+        <iframe src="https://drive.google.com/file/d/1O_OgxQ8aCS0jIWkP140BBkKtNwkfQrEq/preview" width="100%" height="650" allow="autoplay"></iframe>
 	</div>
 </template>
 
